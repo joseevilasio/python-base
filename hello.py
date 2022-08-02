d06be009163690988c31a5876cb39a4e64c0e579
josevilasio <jose_evilasio@outlook.com>
2022-08-01 23:16:49 -0300

    adicionando estrutura de dados dict e removendo complexidade

--- hello.py
+++ hello.py
@@ -16,7 +16,7 @@ Execucao
    ou
    ./hello.py   
 """
-__version__ = "0.0.1"
+__version__ = "0.1.2"
 __author__ = "Jose Junior"
 __license__ = "Unlincense"
 
@@ -24,15 +24,12 @@ import os
 
 current_language = os.getenv("LANG", "en_US")[:5]
 
-msg = "Hello, World!"
+msg = {
+		"en_US": "Hello, World!",
+		"pt_BR": "Ola, Mundo!",
+		"it_IT": "Ciao, Mondo!",
+		"es_SP": "Hola, Mundo!",
+		"fr_FR":"Bonjour Monde",
+}
 
-if current_language == "pt_BR":
-   msg = "Ola, Mundo!"
-elif current_language == "it_IT":
-   msg = "Ciao, Mondo!"
-elif current_language == "es_SP":   
-   msg = "Hola, Mundo!"
-elif current_language == "fr_FR":
-   msg = "Bonjour Monde"
-         
-print(msg)
+print(msg.get(current_language,"Hello, World!"))
