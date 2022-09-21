4f98a873bbc42aff42b03895addb90b93abb82d7
joseevilasio <jose_evilasio@outlook.com>
2022-09-21 18:37:10 -0300

    retirado template e ajustado formatação

--- tabuada.py
+++ tabuada.py
@@ -11,19 +11,12 @@
 __version__ = "0.1.1"
 __author__ = "Jose Junior"
 
-from ctypes import alignment
-
-
 numbers = list(range(1, 11))
-template = """
---- Tabela de {number} ---
-    {block:>}
-#################
-"""
 
 for number in numbers:
-    block = ""
+    print("{:-^18}".format(f"Tabela do {number}"))
+    print()
     for other_number in numbers:
-        operation = f"{number} x {other_number} = {(number * other_number)}\n"
-        block += operation
-        print(template.format(number=number, block=block))
+        result = number * other_number
+        print("{:^18}".format(f"{number} x {other_number} = {result}"))
+    print("{:#^18}".format(""))
