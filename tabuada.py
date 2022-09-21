44c57afe74a0d6ff7b52ce3d04acf496fd3278ab
joseevilasio <jose_evilasio@outlook.com>
2022-09-21 11:57:37 -0300

    formatando texto

--- tabuada.py
+++ tabuada.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#! /usr/bin/env python
 """Imprime a tabuada do 1 ao 10
 Tabuada 1
 
@@ -8,13 +8,12 @@ Tabuada 1
 __version__ = "0.1.0"
 __author__ = "Jose Junior"
 
-#base = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
 numbers = list(range(1, 11))
 
 for number in numbers:
 
-    print(f"Tabuada do número: {number}")
+    print(f"Tabuada do número: {number}\n")
     for other_number in numbers:
-        print(f"{number * other_number}")
+        print(
+            f"Número {number} vezes {other_number} = {number * other_number}")
     print("_" * 40)
