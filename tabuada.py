41f9a242f8c94117066f4840d38ce2939e1204ce
joseevilasio <jose_evilasio@outlook.com>
2022-09-20 21:26:08 -0300

    criação do script tabuada

---
+++ tabuada.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+"""Imprime a tabuada do 1 ao 10
+Tabuada 1
+
+1 x 1 = 1
+
+"""
+__version__ = "0.1.0"
+__author__ = "Jose Junior"
+
+base = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(base)
