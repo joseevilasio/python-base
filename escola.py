bc35dc07677e3ebb6ecba5804e0926bed912a403
joseevilasio <jose_evilasio@outlook.com>
2022-09-23 11:25:33 -0300

    base de dados para exercicio

--- escola.py
+++ escola.py
@@ -6,3 +6,10 @@ que frequenta cada uma das atividades.
 """
 __version__ = "0.1.0"
 __author__ = "Jose Junior"
+
+sala1 = ["Erik", "Maia", "Gustavo", "Manoel", "Sofia", "Joana"]
+sala2 = ["Joao", "Antonio", "Carlos", "Maria", "Isolda"]
+
+aula_ingles = ["Erik", "Maia", "Joana", "Carlos", "Antonio"]
+aula_musica = ["Erik", "Carlos", "Maria"]
+aula_danca = ["Gustavo", "Sofia", "Joana", "Antonio"]
