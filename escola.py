2659d80616e35bd9a0c4698d7e48ad43d56ccfb9
joseevilasio <jose_evilasio@outlook.com>
2022-09-23 11:13:18 -0300

    criado arquivo escola

---
+++ escola.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+Exibe relatório de crianças por atividade.
+Imprimir a lista de crianças agrupadas por sala
+que frequenta cada uma das atividades.
+"""
+__version__ = "0.1.0"
+__author__ = "Jose Junior"
