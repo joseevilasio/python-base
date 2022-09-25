0491e9a99de1325928d9709a184d8a7765915f8a
joseevilasio <jose_evilasio@outlook.com>
2022-09-25 18:11:17 -0300

    refatorando com dict

--- escola_com_dict.py
+++ escola_com_dict.py
@@ -7,25 +7,23 @@ que frequenta cada uma das atividades.
 __version__ = "0.1.1"
 __author__ = "Jose Junior"
 
-sala1 = ["Erik", "Maia", "Gustavo", "Manoel", "Sofia", "Joana"]
-sala2 = ["Joao", "Antonio", "Carlos", "Maria", "Isolda"]
-
-aula_ingles = ["Erik", "Maia", "Joana", "Carlos", "Antonio"]
-aula_musica = ["Erik", "Carlos", "Maria"]
-aula_danca = ["Gustavo", "Sofia", "Joana", "Antonio"]
+salas = {
+    "sala1" : ["Erik", "Maia", "Gustavo", "Manoel", "Sofia", "Joana"],
+    "sala2" : ["Joao", "Antonio", "Carlos", "Maria", "Isolda"],
+}
 
 atividades = {
-    "Inglês": aula_ingles,
-    "Música": aula_musica,
-    "Dança": aula_danca,
+    "Inglês": ["Erik", "Maia", "Joana", "Carlos", "Antonio"],
+    "Música": ["Erik", "Carlos", "Maria"],
+    "Dança": ["Gustavo", "Sofia", "Joana", "Antonio"],
 }
 
 for nome, atividade in atividades.items():    
     print(f"Aula de {nome}")
     print() 
 
-    atividade_sala1 = set(sala1).intersection(set(atividade))
-    atividade_sala2 = set(sala2).intersection(set(atividade))
+    atividade_sala1 = set(salas["sala1"]).intersection(set(atividade))
+    atividade_sala2 = set(salas["sala2"]).intersection(set(atividade))
 
     print("Sala 01: ", atividade_sala1)
     print("Sala 02: ", atividade_sala2)
