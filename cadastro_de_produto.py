18c3a582984ad877abb9aca8a5d2131f03277f5c
joseevilasio <jose_evilasio@outlook.com>
2022-09-25 13:19:21 -0300

    adicionando o arquivo para exercicio dict

---
+++ cadastro_de_produto.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Cadastro de produto
+"""
+__version__ = "0.1.0"
+__author__ = "Jose Junior"
+
+produto_nome = "Caneta"
+produto_cor1 = "azul"
+produto_cor2 = "branco"
+produto_preco = 3.23
+produto_dimensao_altura = 12.1
+produto_dimensao_largura = 0.8
+produto_em_estoque = True
+produto_codigo = 45678
+produto_codebar = None
