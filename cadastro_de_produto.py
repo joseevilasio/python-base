5ed947391b87c116010e017f21318ec8504a7048
joseevilasio <jose_evilasio@outlook.com>
2022-09-25 17:21:01 -0300

    inserindo dict

--- cadastro_de_produto.py
+++ cadastro_de_produto.py
@@ -5,12 +5,20 @@ Cadastro de produto
 __version__ = "0.1.0"
 __author__ = "Jose Junior"
 
-produto_nome = "Caneta"
-produto_cor1 = "azul"
-produto_cor2 = "branco"
-produto_preco = 3.23
-produto_dimensao_altura = 12.1
-produto_dimensao_largura = 0.8
-produto_em_estoque = True
-produto_codigo = 45678
-produto_codebar = None
+produto = {
+    "nome": "Caneta",
+    "cor1": "azul",
+    "cor2": "branco",
+    "preco": 3.23,
+    "dimensao_altura": 12.1,
+    "dimensao_largura":  0.8,
+    "em_estoque": True,
+    "codigo": 45678,
+    "codebar": None,
+}
+
+compra = ("Bruno", produto["nome"], 3)
+total_compra = compra[2] * produto["preco"]
+
+print(
+    f"O Cliente {compra[0]} comprou {compra[1]} e pagou o total de {total_compra}")
