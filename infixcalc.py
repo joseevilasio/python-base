e50d766c411f194a417f845ed0e3ea90193809a5
joseevilasio <jose_evilasio@outlook.com>
2022-09-25 21:31:29 -0300

    adicionado arquivo para exercicio

---
+++ infixcalc.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Calculadora infix.
+
+Funcionamento:
+
+[operação] [n1] [n2]
+
+Operações:
+sum -> +
+sub -> -
+mul -> *
+div -> /
+
+Uso:
+$ infixcalc.py sum 5 2
+7
+
+$ infixcalc.py mul 10 5
+50
+
+$ infixcalc.py
+operação: sum
+n1:5
+n2:4
+9
+"""
+__version__ = "0.1.0"
