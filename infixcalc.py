e085921e4b49881d0a32f604f7867a3d6a31c43e
joseevilasio <jose_evilasio@outlook.com>
2022-09-25 22:13:01 -0300

    adicionando input

--- infixcalc.py
+++ infixcalc.py
@@ -25,3 +25,38 @@ n2:4
 9
 """
 __version__ = "0.1.0"
+__author__ = "Jose Junior"
+
+import sys
+
+arguments = {
+    "operation": None,
+    "n1": None,
+    "n2": None,
+}
+
+if len(sys.argv[1:]) < 1:
+    arguments["operation"] = input(
+        "Insert operantion 'sum' 'sub' 'mul' 'div': ").strip()
+    arguments["n1"] = int(input("Insert number: "))
+    arguments["n2"] = int(input("Insert number: "))
+
+# Operação
+
+if arguments["operation"] == "sum":
+    result = arguments["n1"] + arguments["n2"]
+elif arguments["operation"] == "sub":
+    result = arguments["n1"] - arguments["n2"]
+elif arguments["operation"] == "mul":
+    result = arguments["n1"] * arguments["n2"]
+elif arguments["operation"] == "div":
+    result = arguments["n1"] / arguments["n2"]
+
+# Resultado
+
+print(
+    f"Operação: {arguments['operation']}\n "
+    f"n1: {arguments['n1']}\n "
+    f"n2: {arguments['n2']}\n "
+    f"{result}"
+)
