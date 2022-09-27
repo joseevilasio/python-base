d623d82ed809101c2035c8b2959a4029a1f0d5ba
joseevilasio <jose_evilasio@outlook.com>
2022-09-26 22:04:14 -0300

    refatorando add feature buscar arquivo

--- email_template.py
+++ email_template.py
@@ -1,21 +1,38 @@
-email_template = """
-Olá, {nome}
-Tem interesse em comprar {produto}?
-Este produto é ótimo para resolver
-{texto}
-Clique agora em {link}
-
-Apenas {quantidade} disponiveis!
-Preço promocional {preco:.2f}
+#!/usr/bin/env python3
+"""Faz envio de emails via arquivo de banco de dados e template
 """
+__version__ = "0.1.2"
+__author__ = "Jose Junior"
 
-clientes = ["Maria", "Joao", "Bruno"]
+import sys
+import os
 
-for cliente in clientes:
+# Input argv
+arguments = sys.argv[1:]
+if not arguments:
+    print("informe o nome do arquivo de emails")
+    sys.exit(1)
+
+filename = arguments[0]
+
+# Input file
+
+path = os.curdir
+filepath = os.path.join(path, filename)
+filetemplate = os.path.join(path, "template.txt")
+clientes = []
+
+for line in open(filepath):
+    # TODO: Substituir por list comprehension
+    clientes.append(line.split(","))
+
+for name, email in clientes:
+    # TODO: Substituir por envio de email
+    print(f"--> Enviando para o email: {email} <--\n")
     print(
-        email_template.format
+        open(filetemplate).read().format
         (
-            nome=cliente,
+            nome=name,
             produto="caneta",
             texto="Escrever muito bem",
             link="https://caneta.com.br",
