f2d341db111ce12c4ed366d36ee822c5173fe1b7
joseevilasio <jose_evilasio@outlook.com>
2022-09-27 10:38:17 -0300

    adicionado arquivo para exercicio

---
+++ bloco_de_notas.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""
+Bloco de Notas
+
+$ bloco_de_notas.py new "Minha Nota"
+tag: tecnologia
+text:
+
+Após enter arquivo é salvo minha_nota.txt
+
+$ bloco_de_notas.py read --tag=tecnologia
+...
+"""
+
+__version__ = "0.1.0"
+__author__ = "Jose Junior"
