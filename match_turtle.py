e97b3f5f2229c13fa9df28d94c2c06611c8df790
joseevilasio <jose_evilasio@outlook.com>
2022-12-12 10:08:50 -0300

    Pattern Matching example

--- match_turtle.py
+++ match_turtle.py
@@ -1,5 +1,5 @@
 from turtle import Turtle
-import tkinter as TK
+
 
 turtle = Turtle()
 turtle.shape("turtle")
@@ -9,15 +9,36 @@ turtle.penup()
 
 while True:
     command = input(">").strip().split()
-    # move 2 3 - ["move", "2", "3"]
-    if command[0] in ("exit", "stop"):
-        break
-    if command[0] == "draw":
-        # draw line 40
-        shape = command[1]
-        size = float(command[2])
-        turtle.pendown()
-        if shape == "line":
-            turtle.forward(size)
-        elif shape == "circle":
-            turtle.circle(size)
+    
+    match command:
+
+        case ["move", *points]:
+            match points:
+                case [x, y]:
+                    turtle.goto(float(x), float(y))
+                case [steps]:
+                    turtle.forward(float(steps))
+
+        case ["turn", *options]:
+            match options:
+                case [angle]:
+                    turtle.right(float(angle))
+                case _:
+                    turtle.right(90)
+        
+        case ["draw", shape, size]:
+            turtle.pendown
+            match shape:
+                case "circle":
+                    turtle.circle(float(size))
+                case "line":
+                    turtle.forward(float(size))
+            turtle.penup()
+        
+        case ["write", *text]:
+            turtle.write(" ".join(text), None, "center", "16pt 20")
+        
+        case ["exit", "stop", "quit"]:
+            break
+        case _:
+            print("Invalid command")
