e684d3ee1efd6074d21e6a39eb091a495ce64907
joseevilasio <jose_evilasio@outlook.com>
2023-02-23 21:46:23 -0300

    usando yield

--- exemplo_yields.py
+++ exemplo_yields.py
@@ -1,9 +1,30 @@
+def other_math(number):
+  print('do the math with:', number)
+  
+def gen_squares_in_interval(x, y):
+  for i in range(x, y):
+    yield i*i
+
+if __name__ == '__main__':
+
+  x, y = (10, 20)
+  
+  for number in gen_squares_in_interval(x, y):
+    other_math(number)
 
 
-list_compr = [x for x in range(10)]
-print(list_compr)
-# >> [0, 1, 2, 3, 4, 5, 6, 7, 9, 10]
 
-list_gener = (x for x in range(10))
-print(next(list_gener))
-# >> <generator object <genexpr> at 0x7fcd403145c8>
+def other_math(number):
+  print('do the math with:', number)
+  
+def gen_squares_in_interval(x, y):
+  for i in range(x, y):
+    return i*i
+
+if __name__ == '__main__':
+
+  x, y = (10, 20)
+  
+  for _ in range(x, y):
+    number = gen_squares_in_interval(x, y)
+    other_math(number)
